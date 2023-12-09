10fa83b37552a84dab534a1e2fec3a99924d23ef
Alonso8729 <alon8729@gmail.com>
2023-12-09 10:57:07 +0200

    Added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,53 @@
-import { useState } from 'react'
 import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+import { Products } from "./pages/Products/Products";
+import { Product } from "./pages/Product/Product";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Footer } from "./components/Footer/Footer";
+import { Home } from "./pages/Home/Home";
+
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/clothing-store/',
+      element: <Layout />,
+      children: [
+        {
+          path: '',
+          element: <Home />
+        },
+        {
+          path: 'products/:id',
+          element: <Products />
+        },
+        {
+          path: 'product/:id',
+          element: <Product />
+        }
+      ]
+    },
+  ]
+)
 
 function App() {
-  return
-
+  return (
+    <RouterProvider router={router} />
+  )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
-const router = createBrowserRouter([
-    {
-        path: '/clothing-store/',
-        element: <App />
-    },
-    {
-        path: '/clothing-store/products/',
-        element: <span>Category</span>
-    },
-    {
-        path: '/clothing-store/product/',
-        element: <span>Product</span>
-    }
-])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <App />
     </React.StrictMode>
 );
