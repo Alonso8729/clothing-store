55a51c2fca15c628fe4954f5bfc06e2c1e0523a9
Alonso8729 <alon8729@gmail.com>
2023-12-09 18:12:36 +0200

    Implemented components and styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,52 @@
 import React from 'react'
+import styles from './Footer.module.css'
+import { Link } from 'react-router-dom'
+import payment from '../../assets/img/payment.png'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.footer}>
+      <div className={styles.top}>
+        <div className={styles.item}>
+          <h3>Categories</h3>
+          <Link className={styles.linkStyle}>Women</Link>
+          <Link className={styles.linkStyle}>Men</Link>
+          <Link className={styles.linkStyle}>Shoes</Link>
+          <Link className={styles.linkStyle}>Accessories</Link>
+          <Link className={styles.linkStyle}>New Arrivals</Link>
+        </div>
+        <div className={styles.item}>
+          <h3>Links</h3>
+          <Link className={styles.linkStyle}>FAQ</Link>
+          <Link className={styles.linkStyle}>Pages</Link>
+          <Link className={styles.linkStyle}>Coockies</Link>
+        </div>
+        <div className={styles.item}>
+          <h3>About</h3>
+          <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Reiciendis asperiores distinctio nam pariatur?
+            Vitae ab facere tempore repudiandae adipisci odit necessitatibus facilis modi est!
+            Itaque quasi provident officiis esse repellat?</span>
+        </div>
+        <div className={styles.item}>
+          <h3>Contact</h3>
+          <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Reiciendis asperiores distinctio nam pariatur?
+            Vitae ab facere tempore repudiandae adipisci odit necessitatibus facilis modi est!
+            Itaque quasi provident officiis esse repellat?</span>
+        </div>
+      </div>
+      <div className={styles.bottom}>
+        <div className={styles.left}>
+          <span className={styles.logo}>Trendify </span>
+          <span className={styles.copyright}>
+            ©Copyright 2023. All Rights Reserved
+          </span>
+        </div>
+        <div className={styles.right}>
+          <img src={payment} alt="payment" />
+        </div>
+      </div>
+    </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,52 @@
 import React from 'react'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined'; import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import en from '../../assets/img/en.png'
+import { Link } from 'react-router-dom'
+import styles from './Navbar.module.css'
 
 export const Navbar = () => {
   return (
-    <div className='navbar'>
-      <div className="wrapper">
-        <div className="left"></div>
-        <div className="center"></div>
-        <div className="right"></div>
+    <div className={styles.navbar}>
+      <div className={styles.wrapper}>
+        <div className={styles.left}>
+          <div className={styles.item}>
+            <img src={en} />
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className={styles.item}>
+            <span>USD</span>
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className={styles.item}>
+            <Link className={styles.linkStyle} to='products/1'>Women</Link>
+          </div>
+          <div className={styles.item}>
+            <Link className={styles.linkStyle} to='products/2'>Men</Link>
+          </div>
+          <div className={styles.item}>
+            <Link className={styles.linkStyle} to='products/3'>Kids</Link>
+          </div>
+        </div>
+        <div className={styles.center}>
+          <Link className={styles.linkStyle} to=''>Trendify</Link>
+        </div>
+        <div className={styles.right}>
+          <Link className={styles.linkStyle} to=''>About</Link>
+          <Link className={styles.linkStyle} to=''>Contact</Link>
+          <div className={styles.icons}>
+            <SearchIcon />
+            <PersonOutlinedIcon />
+            <FavoriteBorderOutlinedIcon />
+            <div className={styles.cartIcon}>
+              <ShoppingCartOutlinedIcon />
+              <span className={styles.itemsCounter}>0</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
