c5a0231b385e07e1cad21b61cc82d8000119c32a
Alonso8729 <alon8729@gmail.com>
2023-12-22 16:25:25 +0200

    Implemented components and styles

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import styles from './Categories.module.css'
+import c1 from '../../assets/Categories/c1.jpg'
+import c2 from '../../assets/Categories/c2.jpg'
+import c3 from '../../assets/Categories/c3.jpg'
+import c4 from '../../assets/Categories/c4.jpg'
+import c5 from '../../assets/Categories/c5.jpg'
+import c6 from '../../assets/Categories/c6.jpg'
+import { Link } from 'react-router-dom'
+
+const Categories = () => {
+    return (
+        <div className={styles.categories}>
+            <div className={styles.col}>
+                <div className={styles.row}>
+                    <img src={c1} className={styles.img} />
+                    <button className={styles.button}>
+                        <Link className='link' to='/products/1'>
+                            Sale
+                        </Link>
+                    </button>
+                </div>
+                <div className={styles.row}>
+                    <img src={c2} className={styles.img} />
+                    <button className={styles.button}>
+                        <Link to='/products/2' className='link'>
+                            Women
+                        </Link>
+                    </button>
+                </div>
+            </div>
+            <div className={styles.col}>
+                <div className={styles.row}>
+                    <img src={c3} className={styles.img} />
+                    <button className={styles.button}>
+                        <Link to='/products/3' className='link'>
+                            New Season
+                        </Link>
+                    </button>
+                </div>
+            </div>
+            <div className={`${styles.col} ${styles.largeCol}`}>
+                <div className={styles.row}>
+                    <div className={styles.col}>
+                        <div className={styles.row}>
+                            <img src={c4} className={styles.img} />
+                            <button className={styles.button}>
+                                <Link to='/products/4' className='link'>
+                                    New Season
+                                </Link>
+                            </button>
+                        </div>
+                    </div>
+                    <div className={styles.col}>
+                        <div className={styles.row}>
+                            <img src={c5} className={styles.img} />
+                            <button className={styles.button}>
+                                <Link to='/products/5' className='link'>
+                                    Accessorries
+                                </Link>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.row}>
+                    <img src={c6} className={styles.img} />
+                    <button className={styles.button}>
+                        <Link to='/products/6' className='link'>
+                            Shoes
+                        </Link>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Categories
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import styles from './Contact.module.css'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GoogleIcon from '@mui/icons-material/Google';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+
+
+const Contact = () => {
+    return (
+        <div className={styles.contact}>
+            <div className={styles.wrapper}>
+                <span>BE IN TOUCH WITH US: </span>
+                <div className={styles.mail}>
+                    <input className={styles.input} type="text" placeholder='Enter youe email' />
+                    <button className={styles.btn}>JOIN US</button>
+                </div>
+                <div className={styles.icons}>
+                    <FacebookIcon/>
+                    <InstagramIcon/>
+                    <TwitterIcon/>
+                    <GoogleIcon/>
+                    <PinterestIcon/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
