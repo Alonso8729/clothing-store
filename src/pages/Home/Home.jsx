f50579c08e76cd965a45aeb6f218685fd04c357a
Alonso8729 <alon8729@gmail.com>
2023-12-22 16:25:54 +0200

    Added components to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import Slider from '../../components/Slider/Slider'
 import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
+import Categories from '../../components/Categories/Categories'
+import Contact from '../../components/Contact/Contact'
 export const Home = () => {
   return (
     <div>
       <Slider />
       <FeaturedProducts type='Featured' />
+      <Categories/>
       <FeaturedProducts type='Trending' />
+      <Contact/>
     </div>
   )
 }
