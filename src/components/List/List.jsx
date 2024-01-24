375bca038ad589d9e665acd5d464562eda3c6469
Alonso8729 <alon8729@gmail.com>
2024-01-24 14:49:38 +0200

    Added new props and data filtering

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -3,8 +3,23 @@ import styles from './List.module.css'
 import Card from '../Card/Card'
 import data from '../../Data.jsx'
 
-const List = ({ catId }) => {
-    const filteredData = data.filter(item => item.catId === catId)
+const List = ({ catId, maxPrice, sort, subCats }) => {
+    let filteredData = data.filter(item => item.catId === catId && item.newPrice <= maxPrice)
+
+    if (sort) { // The user choose to sort items by price
+        filteredData.sort((a, b) => {
+            if (sort === 'asc')
+                return a.newPrice - b.newPrice;
+            else if (sort === 'desc')
+                return b.newPrice - a.newPrice;
+            else
+                return 0;
+        })
+    }
+
+    if (subCats.length > 0) {
+        filteredData = filteredData.filter(item => subCats.includes(item.subCat));
+    }
 
     return (
         <div className={styles.list}>
