0267695565605df8bde2da1981edd8ed920cb31e
Alonso8729 <alon8729@gmail.com>
2024-01-26 17:55:43 +0200

    Updated initialState variable name

--- src/redux/wishlistReducer.js
+++ src/redux/wishlistReducer.js
@@ -1,28 +1,26 @@
-import { createSlice } from '@reduxjs/toolkit'
+// Inside your wishlistReducer.js
+import { createSlice } from '@reduxjs/toolkit';
 
-const wishlistSlice = createSlice({
-    name: 'wishlist',
-    initialState: {
-        items: [],
+export const wishlistSlice = createSlice({
+  name: 'wishlist',
+  initialState: {
+    products: [],
+  },
+  reducers: {
+    addToWishlist: (state, action) => {
+      const existingItem = state.products.find((item) => item.id === action.payload.id);
+
+      if (existingItem) {
+        existingItem.quantity += action.payload.quantity;
+      } else {
+        state.products.push(action.payload);
+      }
     },
-    reducers: {
-        addToWishlist: (state, action) => {
-            const existingItem = state.items.find(item => item.id === action.payload.id);
+    removeFromWishlist: (state, action) => {
+      state.products = state.products.filter((item) => item.id !== action.payload);
+    },
+  },
+});
 
-            if (existingItem) {
-                // If the item exists, update it
-                item.quantity += action.payload.quantity;
-            } else {
-                // If the item doesn't exist, add it to the wishlist
-                state.items.push(action.payload);
-            }
-        },
-        removeFromWishlist: (state, action) => {
-            state.items = state.items.filter(item => item.id !== action.payload)
-        },
-
-    }
-})
-
-export const { removeFromWishlist, addToWishlist } = wishlistSlice.actions
-export default wishlistSlice.reducer
+export const { removeFromWishlist, addToWishlist } = wishlistSlice.actions;
+export default wishlistSlice.reducer;
