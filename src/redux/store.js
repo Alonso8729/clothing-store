1421dbedc9d4123fb445f0250845f95fb3577f66
Alonso8729 <alon8729@gmail.com>
2024-01-26 17:40:45 +0200

    unused file

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit'
-import cartReducer from './cartReducer'
-import wishlistReducer from './wishlistReducer'
-
-export default combineReducers({
-  cartReducer: {cart:cartReducer},
-  wishlistReducer:{wishlist:wishlistReducer}
-})
