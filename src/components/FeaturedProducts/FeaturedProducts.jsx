8e875f985dcf42270aeb06658a56567b883a4cfe
Alonso8729 <alon8729@gmail.com>
2024-02-04 17:46:29 +0200

    Changed desc

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -16,25 +16,23 @@ import data from '../../Data'
 const FeaturedProducts = ({ type }) => {
 
     //Write const data with couple of featured products
-    const featuredProducts = data.filter(item => item.id >= 1 && item.id<=4);
-    const trendingProducts = data.filter(item=> item.id >=5 && item.id <=8);
+    const featuredProducts = data.filter(item => item.id >= 1 && item.id <= 4);
+    const trendingProducts = data.filter(item => item.id >= 5 && item.id <= 8);
 
-    const selectedProducts = type === 'Featured' ? featuredProducts:trendingProducts
+    const selectedProducts = type === 'Featured' ? featuredProducts : trendingProducts
 
 
     return (
         <div className={styles.featuredProducts}>
             <div className={styles.top}>
                 <h1 className={styles.h1}>{type} Products</h1>
-                <p className={styles.p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi iste dignissimos omnis vitae?
-                    Tempora quas possimus eligendi animi ratione,
-                    id atque esse veniam reprehenderit quo corporis unde eaque fugiat ipsa.</p>
+                <p className={styles.p}>Explore Trendify's featured clothing collection, where contemporary trends meet timeless elegance.</p>
             </div>
             <div className={styles.bottom}>
                 {selectedProducts?.map(item => (
                     <Card item={item} key={item.id} />
                 ))}
-               
+
             </div>
         </div>
     )
