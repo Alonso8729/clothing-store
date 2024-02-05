ddb1aaa852cb68e22cf9cd7efab21a6982a1dc99
Alonso8729 <alon8729@gmail.com>
2024-02-05 19:18:03 +0200

    Added navigation

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,11 +9,10 @@ export const Footer = () => {
       <div className={styles.top}>
         <div className={styles.item}>
           <h3>Categories</h3>
-          <Link className={styles.linkStyle}>Women</Link>
-          <Link className={styles.linkStyle}>Men</Link>
-          <Link className={styles.linkStyle}>Shoes</Link>
-          <Link className={styles.linkStyle}>Accessories</Link>
-          <Link className={styles.linkStyle}>New Arrivals</Link>
+          <Link to='products/2' className={styles.linkStyle}>Women</Link>
+          <Link to='products/4' className={styles.linkStyle}>Men</Link>
+          <Link to='products/5' className={styles.linkStyle}>Accessories</Link>
+          <Link to='products/3' className={styles.linkStyle}>New Arrivals</Link>
         </div>
         <div className={styles.item}>
           <h3>Links</h3>
@@ -23,10 +22,8 @@ export const Footer = () => {
         </div>
         <div className={styles.item}>
           <h3>About</h3>
-          <span className={styles.span}>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Reiciendis asperiores distinctio nam pariatur?
-            Vitae ab facere tempore repudiandae adipisci odit necessitatibus facilis modi est!
-            Itaque quasi provident officiis esse repellat?</span>
+          <span className={styles.span}>Explore Trendify, your go-to fashion hub for curated styles that transcend trends. We believe in empowering self-expression through carefully selected apparel,
+            inspiring confidence for the modern individual. Welcome to Trendify, where fashion meets empowerment.</span>
         </div>
         <div className={styles.item}>
           <h3>Contact</h3>
@@ -40,7 +37,7 @@ export const Footer = () => {
         <div className={styles.left}>
           <span className={styles.logo}>Trendify </span>
           <span className={styles.copyright}>
-            ©Copyright 2023. All Rights Reserved
+            ©Copyright 2024 <a className={styles.alonso} target="_blank" href="https://github.com/Alonso8729/">Alonso.</a>
           </span>
         </div>
         <div className={styles.right}>
